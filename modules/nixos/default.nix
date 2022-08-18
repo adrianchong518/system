78ee3249c097559dccd0699d3b2b5ef60458732f
adrianchong518 <adrianchong518@gmail.com>
2022-08-19 02:53:36 +0800

    Change to gnome-keyring for libsecret

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -44,6 +44,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Gnome keyring
+  services.gnome.gnome-keyring.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
