20d2a506f46b9c6152291b3ceaf2ca2b9d5539ef
adrianchong518 <adrianchong518@gmail.com>
2022-08-18 21:30:35 +0800

    Add git credential helper

--- modules/home-manager/cli/git.nix
+++ modules/home-manager/cli/git.nix
@@ -6,6 +6,12 @@
 
     extraConfig = {
       init.defaultBranch = "main";
+      credential.helper =
+        if pkgs.stdenvNoCC.isDarwin then
+          "osxkeychain"
+        else "${
+          pkgs.git.override { withLibsecret = true; }
+        }/bin/git-credential-libsecret";
     };
 
     aliases = {
