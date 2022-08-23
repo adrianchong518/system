b24aecd2667bd58de44cd135e6d827d7ed7e5cf2
adrianchong518 <adrianchong518@gmail.com>
2022-08-24 01:54:56 +0800

    Move and edit git aliases

--- modules/home-manager/cli/git.nix
+++ modules/home-manager/cli/git.nix
@@ -1,6 +1,26 @@
 { config, lib, pkgs, ... }:
 
 {
+  home.shellAliases = {
+    # git
+    g = "git";
+    gp = "git push";
+    gpo = "git push origin";
+    gpl = "git pull";
+    gplo = "git pull origin";
+    gf = "git fetch";
+    gst = "git status";
+    ga = "git add";
+    gaa = "git add --all";
+    gap = "git add -p";
+    gc = "git commit";
+    gcm = "git commit -m";
+    gcl = "git clone";
+    gb = "git branch";
+    gco = "git checkout";
+    gs = "git stash";
+  };
+
   programs.git = {
     enable = true;
 
