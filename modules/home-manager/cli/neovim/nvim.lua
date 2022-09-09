91affabbdc68ccd8d19a3f79cdab65cda51817fb
adrianchong518 <adrianchong518@gmail.com>
2022-09-09 23:24:45 +0800

    refactor nvim.lua config further

--- modules/home-manager/cli/neovim/nvim.lua
+++ modules/home-manager/cli/neovim/nvim.lua
@@ -1,21 +1,21 @@
 -- Speed up!
-require('impatient')
+require("impatient")
 
 --- General Vim Settings ----------
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.wrap = true
-vim.o.encoding = 'utf-8'
+vim.o.encoding = "utf-8"
 vim.o.wildmenu = true
 vim.o.lazyredraw = true
 vim.o.ruler = true
 vim.o.incsearch = true
 vim.o.hlsearch = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.scrolloff = 5
 vim.o.cursorline = true
 vim.o.updatetime = 300
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 vim.cmd([[
     set nobackup
@@ -24,12 +24,12 @@ vim.cmd([[
 
 -- Strip trailing whitespaces
 function vim.fn.stripTrailingWhitespace()
-    local l = vim.fn.line('.')
-    local c = vim.fn.col('.')
+    local l = vim.fn.line(".")
+    local c = vim.fn.col(".")
     vim.cmd([[%s/\s\+$//e]])
     vim.fn.cursor(l, c)
 end
-vim.cmd('autocmd BufWritePre * :lua vim.fn.stripTrailingWhitespace()')
+vim.cmd("autocmd BufWritePre * :lua vim.fn.stripTrailingWhitespace()")
 
 -- Set indentation
 vim.o.shiftwidth = 4
@@ -39,25 +39,25 @@ vim.o.autoindent = true
 vim.o.smartindent = true
 
 function setTabWidth(filetype, width)
-    vim.cmd(string.format('autocmd FileType %s setlocal shiftwidth=%d softtabstop=%d expandtab', filetype, width, width))
+    vim.cmd(string.format("autocmd FileType %s setlocal shiftwidth=%d softtabstop=%d expandtab", filetype, width, width))
 end
 
-setTabWidth('nix', 2)
-setTabWidth('c', 2)
-setTabWidth('cpp', 2)
-setTabWidth('scheme', 2)
-setTabWidth('haskell', 2)
-setTabWidth('markdown', 2)
+setTabWidth("nix", 2)
+setTabWidth("c", 2)
+setTabWidth("cpp", 2)
+setTabWidth("scheme", 2)
+setTabWidth("haskell", 2)
+setTabWidth("markdown", 2)
 
 -- Plenary
-require'plenary.filetype'.add_file('plenary-types')
+require"plenary.filetype".add_file("plenary-types")
 
 --- UI / Theming ----------
-if vim.fn.empty('$TMUX') then
-    if vim.fn.has('nvim') then
-        vim.cmd('let $NVIM_TUI_ENABLE_TRUE_COLOR=1')
+if vim.fn.empty("$TMUX") then
+    if vim.fn.has("nvim") then
+        vim.cmd("let $NVIM_TUI_ENABLE_TRUE_COLOR=1")
     end
-    if vim.fn.has('termguicolors') then
+    if vim.fn.has("termguicolors") then
         vim.opt.termguicolors = true
     end
 end
@@ -68,13 +68,13 @@ vim.cmd([[
 ]])
 
 -- Lualine
-require('lualine').setup({
-    options = { theme = 'onedark' },
+require("lualine").setup({
+    options = { theme = "onedark" },
 })
 
 --- Panels ----------
 -- nvim-tree
-require('nvim-tree').setup({
+require("nvim-tree").setup({
     view = {
         width = 50,
     },
@@ -87,7 +87,7 @@ require('nvim-tree').setup({
             enable = true,
         },
         icons = {
-            git_placement = 'signcolumn',
+            git_placement = "signcolumn",
         },
     },
 
@@ -97,57 +97,59 @@ require('nvim-tree').setup({
 })
 
 -- Telescope
-require('telescope').load_extension('fzf')
-require('telescope').load_extension('coc')
+require("telescope").load_extension("fzf")
+require("telescope").load_extension("coc")
 
-require('telescope').setup {
+require("telescope").setup {
     defaults = {
-        layout_strategy = 'flex',
+        layout_strategy = "flex",
         layout_config = {
             width = { 0.8, max = 235 },
             height = { 0.95, max = 50 },
-            flip_columns = 150
+            flex = {
+                flip_columns = 150
+            }
         },
         mappings = {
             i ={
-                ['<esc>'] = 'close',
-                ['<C-k>'] = 'move_selection_previous',
-                ['<C-j>'] = 'move_selection_next',
+                ["<esc>"] = "close",
+                ["<C-k>"] = "move_selection_previous",
+                ["<C-j>"] = "move_selection_next",
             },
         },
     },
     extensions = {
         coc = {
-            theme = 'ivy',
+            theme = "ivy",
             prefer_locations = true,
         },
     },
 }
 
 --- Editor Features ----------
-require('nvim-surround').setup()
-require('Comment').setup()
-require('nvim-autopairs').setup {}
+require("nvim-surround").setup()
+require("Comment").setup()
+require("nvim-autopairs").setup {}
 
-require('indent_blankline').setup {
+require("indent_blankline").setup {
     use_treesitter = true,
     show_current_context = true,
 }
-vim.cmd('highlight IndentBlanklineContextChar guifg=#A8A8A8 gui=nocombine')
+vim.cmd("highlight IndentBlanklineContextChar guifg=#A8A8A8 gui=nocombine")
 
 --- Language / LSP ----------
 -- Tree Sitter
-require('nvim-treesitter.configs').setup({
+require("nvim-treesitter.configs").setup({
     highlight = {
         enable = true,
     },
     incremental_selection = {
         enable = true,
         keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
         },
     },
     indent = {
@@ -155,91 +157,91 @@ require('nvim-treesitter.configs').setup({
     },
 })
 
-require'treesitter-context'.setup{
+require"treesitter-context".setup{
     patterns = {
         rust = {
-            'impl_item',
-            'struct',
-            'enum',
-            'mod',
+            "impl_item",
+            "struct",
+            "enum",
+            "mod",
         }
     }
 }
 
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevelstart = 99
 
 --- Keymappings ----------
-vim.keymap.set('n', [[\\]], ':noh<CR>')
+vim.keymap.set("n", [[\\]], ":noh<CR>")
 
 -- coc --
-vim.keymap.set('n', '[g', '<Plug>(coc-diagnostic-prev)', { silent = true })
-vim.keymap.set('n', ']g', '<Plug>(coc-diagnostic-next)', { silent = true })
+vim.keymap.set("n", "[g", "<Plug>(coc-diagnostic-prev)", { silent = true })
+vim.keymap.set("n", "]g", "<Plug>(coc-diagnostic-next)", { silent = true })
 
-vim.keymap.set('n', 'K', ':call ShowDocumentation()<cr>', { silent = true })
+vim.keymap.set("n", "K", ":call ShowDocumentation()<cr>", { silent = true })
 
 -- Use <c-space> to trigger completion.
-vim.keymap.set('i', '<c-space>', 'coc#refresh()', { silent = true, expr = true })
+vim.keymap.set("i", "<c-space>", "coc#refresh()", { silent = true, expr = true })
 
 -- GoTo code navigation.
-vim.keymap.set('n', 'gd', ':Telescope coc definitions<cr>', { silent = true})
-vim.keymap.set('n', 'gy', ':Telescope coc type-definitions<cr>', { silent = true})
-vim.keymap.set('n', 'gi', ':Telescope coc implementations<cr>', { silent = true})
-vim.keymap.set('n', 'gr', ':Telescope coc references<cr>', { silent = true})
+vim.keymap.set("n", "gd", ":Telescope coc definitions<cr>", { silent = true})
+vim.keymap.set("n", "gy", ":Telescope coc type-definitions<cr>", { silent = true})
+vim.keymap.set("n", "gi", ":Telescope coc implementations<cr>", { silent = true})
+vim.keymap.set("n", "gr", ":Telescope coc references<cr>", { silent = true})
 
--- Selection ranges (Requires 'textDocument/selectionRange' support of language server.)
-vim.keymap.set({'n', 'x'}, '<C-s>', '<Plug>(coc-range-select)', { silent = true })
+-- Selection ranges (Requires "textDocument/selectionRange" support of language server.)
+vim.keymap.set({"n", "x"}, "<C-s>", "<Plug>(coc-range-select)", { silent = true })
 
 -- Map function and class text objects
--- NOTE: Requires 'textDocument.documentSymbol' support from the language server.
-vim.keymap.set({'x', 'o'}, 'if', '<Plug>(coc-funcobj-i)')
-vim.keymap.set({'x', 'o'}, 'af', '<Plug>(coc-funcobj-a)')
-vim.keymap.set({'x', 'o'}, 'ic', '<Plug>(coc-classobj-i)')
-vim.keymap.set({'x', 'o'}, 'ac', '<Plug>(coc-classobj-a)')
+-- NOTE: Requires "textDocument.documentSymbol" support from the language server.
+vim.keymap.set({"x", "o"}, "if", "<Plug>(coc-funcobj-i)")
+vim.keymap.set({"x", "o"}, "af", "<Plug>(coc-funcobj-a)")
+vim.keymap.set({"x", "o"}, "ic", "<Plug>(coc-classobj-i)")
+vim.keymap.set({"x", "o"}, "ac", "<Plug>(coc-classobj-a)")
 
 -- Toggle --
-vim.keymap.set('n', '<space>to', ':TagbarToggle<cr>', { silent = true })
-vim.keymap.set('n', '<space>tO', ':TagbarOpenAutoClose<cr>', { silent = true })
-vim.keymap.set('n', '<space>te', ':lua require"nvim-tree".toggle(false, true)<cr>', { silent = true })
-vim.keymap.set('n', '<space>tE', ':NvimTreeFocus<cr>', { silent = true })
+vim.keymap.set("n", "<space>to", ":TagbarToggle<cr>", { silent = true })
+vim.keymap.set("n", "<space>tO", ":TagbarOpenAutoClose<cr>", { silent = true })
+vim.keymap.set("n", "<space>te", function() require"nvim-tree".toggle(false, true) end, { silent = true })
+vim.keymap.set("n", "<space>tE", ":NvimTreeFocus<cr>", { silent = true })
 
 -- Find --
-vim.keymap.set('n', '<space>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>')
-vim.keymap.set('n', '<space>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>')
-vim.keymap.set('n', '<space>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>')
-vim.keymap.set('n', '<space>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>')
+vim.keymap.set("n", "<space>ff", function() require("telescope.builtin").find_files() end)
+vim.keymap.set("n", "<space>fg", function() require("telescope.builtin").live_grep() end)
+vim.keymap.set("n", "<space>fb", function() require("telescope.builtin").buffers() end)
+vim.keymap.set("n", "<space>fh", function() require("telescope.builtin").help_tags() end)
 
 -- Buffer --
-vim.keymap.set('n', '<space>bb', '<cmd>lua require("telescope.builtin").buffers()<cr>')
+vim.keymap.set("n", "<space>bb", function() require("telescope.builtin").buffers() end)
 
 -- Code --
 -- Symbol renaming
-vim.keymap.set('n', '<space>cr', '<Plug>(coc-rename)')
+vim.keymap.set("n", "<space>cr", "<Plug>(coc-rename)")
 
 -- Formatting selected code
-vim.keymap.set({'x', 'n'}, '<space>cf', '<Plug>(coc-format-selected)')
-vim.keymap.set('n', '<space>cF', ':Format<cr>')
+vim.keymap.set({"x", "n"}, "<space>cf", "<Plug>(coc-format-selected)")
+vim.keymap.set("n", "<space>cF", ":Format<cr>")
 
 -- Applying codeAction
-vim.keymap.set({'x', 'n'}, '<space>ca', '<Plug>(coc-codeaction-selected)')
-vim.keymap.set('n', '<space>cA', '<Plug>(coc-codeaction)')
+vim.keymap.set({"x", "n"}, "<space>ca", "<Plug>(coc-codeaction-selected)")
+vim.keymap.set("n", "<space>cA", "<Plug>(coc-codeaction)")
 
 -- AutoFix
-vim.keymap.set('n', '<space>cq', '<Plug>(coc-fix-current)')
+vim.keymap.set("n", "<space>cq", "<Plug>(coc-fix-current)")
 
 -- Code lens
-vim.keymap.set('n', '<space>cl', '<Plug>(coc-codelens-action)')
+vim.keymap.set("n", "<space>cl", "<Plug>(coc-codelens-action)")
 
 -- Diagnostics
-vim.keymap.set('n', '<space>cd', ':Telescope coc diagnostics<cr>', { silent = true, nowait = true })
-vim.keymap.set('n', '<space>cD', ':Telescope coc workspace_diagnostics<cr>', { silent = true, nowait = true })
+vim.keymap.set("n", "<space>cd", ":Telescope coc diagnostics<cr>", { silent = true, nowait = true })
+vim.keymap.set("n", "<space>cD", ":Telescope coc workspace_diagnostics<cr>", { silent = true, nowait = true })
 
 -- Symbols
-vim.keymap.set('n', '<space>co', ':Telescope coc document_symbols<cr>', { silent = true, nowait = true })
-vim.keymap.set('n', '<space>cs', ':Telescope coc workspace_symbols<cr>', { silent = true, nowait = true })
+vim.keymap.set("n", "<space>co", ":Telescope coc document_symbols<cr>", { silent = true, nowait = true })
+vim.keymap.set("n", "<space>cs", ":Telescope coc workspace_symbols<cr>", { silent = true, nowait = true })
 
 -- Misc
-vim.keymap.set('n', '<space>ce', ':<C-u>CocList extensions<cr>', { silent = true, nowait = true })
-vim.keymap.set('n', '<space>cc', ':Telescope coc commands<cr>', { silent = true, nowait = true })
+vim.keymap.set("n", "<space>ce", ":<C-u>CocList extensions<cr>", { silent = true, nowait = true })
+vim.keymap.set("n", "<space>cc", ":Telescope coc commands<cr>", { silent = true, nowait = true })
 
