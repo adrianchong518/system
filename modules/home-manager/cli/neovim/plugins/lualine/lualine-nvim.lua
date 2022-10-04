e9cb5d04cddd056ef135f8197691dbd22b5f5f37
Adrian Chong <adrianchong518@gmail.com>
2022-10-04 20:58:49 +0800

    feat(neovim): show relative filepath in lualine

--- modules/home-manager/cli/neovim/plugins/lualine/lualine-nvim.lua
+++ modules/home-manager/cli/neovim/plugins/lualine/lualine-nvim.lua
@@ -1,3 +1,19 @@
 require("lualine").setup({
     options = { theme = "onedark" },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            },
+        },
+    },
+    inactive_sections = {
+        lualine_c = {
+            {
+                'filename',
+                path = 1,
+            },
+        },
+    },
 })
