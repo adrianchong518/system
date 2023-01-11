5781ce7ce7283fb845e5c6252e1ab5c6cfc42178
Adrian Chong <adrianchong518@gmail.com>
2023-01-11 21:41:13 +0800

    fix(darwin): remove need for application copying work around

--- modules/darwin/core.nix
+++ modules/darwin/core.nix
@@ -29,42 +29,4 @@ in
 
   # Auto upgrade nix package and the daemon service.
   services.nix-daemon.enable = true;
-
-  # see https://github.com/nix-community/home-manager/issues/1341
-  system.activationScripts.applications.text = pkgs.lib.mkForce (
-    ''
-      echo "[nix-darwin] Setting up ~/Applications..." >&2
-      rm -rf ~/Applications/Nix\ Apps
-      mkdir -p ~/Applications/Nix\ Apps
-      for app in $(find ${config.system.build.applications}/Applications -maxdepth 1 -type l); do
-        src="$(/usr/bin/stat -f%Y "$app")"
-        cp -r "$src" ~/Applications/Nix\ Apps
-      done
-    ''
-  );
-
-  hm = { config, pkgs, lib, ... }: {
-    home.activation = {
-      copyApplications =
-        let
-          apps = pkgs.buildEnv {
-            name = "home-manager-applications";
-            paths = config.home.packages;
-            pathsToLink = "/Applications";
-          };
-        in
-        lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-          baseDir="/Applications/Home Manager Apps"
-          if [ -d "$baseDir" ]; then
-            rm -rf "$baseDir"
-          fi
-          mkdir -p "$baseDir"
-          for appFile in ${apps}/Applications/*; do
-            target="$baseDir/$(basename "$appFile")"
-            $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
-            $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
-          done
-        '';
-    };
-  };
 }
