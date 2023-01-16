72d59636810a6a76729ffc9a7a544d61ce48b347
Adrian Chong <adrianchong518@gmail.com>
2023-01-16 22:30:35 +0800

    feat(darwin): add touch id for sudo in screen/tmux

--- modules/_darwin/security.nix
+++ modules/_darwin/security.nix
@@ -4,13 +4,38 @@ with lib;
 with lib.my;
 let
   cfg = config.modules.darwin.security;
+
+  mkSudoTouchIdAuthScript = isEnabled:
+    let
+      file = "/etc/pam.d/sudo";
+      optionText = "# nix system: modules.darwin.security.enableSudoTouchIdAuth";
+      sed = "${pkgs.gnused}/bin/sed";
+    in
+    if isEnabled then ''
+      # Enable sudo Touch ID authentication, if not already enabled
+      if ! grep 'pam_tid.so' ${file} > /dev/null; then
+        ${sed} -i '2i\
+      auth       optional       ${pkgs.pam-reattach}/lib/pam/pam_reattach.so ${optionText}\
+      auth       sufficient     pam_tid.so ${optionText}
+        ' ${file}
+      fi
+    '' else ''
+      # Disable sudo Touch ID authentication, if added by nix-darwin
+      if grep '${optionText}' ${file} > /dev/null; then
+        ${sed} -i '/${optionText}/d' ${file}
+      fi
+    '';
 in
 {
   options.modules.darwin.security = with types; {
     enableSudoTouchIdAuth = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enableSudoTouchIdAuth {
-    security.pam.enableSudoTouchIdAuth = true;
+  config = {
+    system.activationScripts.pam.text = ''
+      # PAM settings
+      echo >&2 "setting up pam..."
+      ${mkSudoTouchIdAuthScript cfg.enableSudoTouchIdAuth}
+    '';
   };
 }
