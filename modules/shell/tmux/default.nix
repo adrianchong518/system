90f368b82908828245f4daee37b2fe9f4c6ffd36
Adrian Chong <adrianchong518@gmail.com>
2023-01-26 20:44:39 +0800

    feat(tmux): add better-mouse-mode, fzf-tmux-url, customize status bar

--- modules/shell/tmux/default.nix
+++ modules/shell/tmux/default.nix
@@ -17,17 +17,56 @@ in
       shortcut = "a";
       terminal = "screen-256color";
       keyMode = "vi";
-      historyLimit = 10000;
+      historyLimit = 1000000;
       clock24 = true;
       customPaneNavigationAndResize = true;
       escapeTime = 0;
 
       extraConfig = ''
-        ${readFile ./gruvbox-material/dark-hard.conf}
-
         set -ga terminal-overrides ",*256col*:Tc"
         set -g mouse on
+        set -g renumber-windows on
+
+        set -g status-interval 2
+        set -g status-left-length 200
+        set -g status-position top
+
+        set -g status-left "#[fg=blue,bold]#S#[fg=white,nobold]#(${pkgs.gitmux}/bin/gitmux -timeout 200ms -cfg ${./gitmux.yml} \"#{pane_current_path}\") "
+        set -g status-right ""
+
+        set -g pane-active-border-style     "fg=magenta,bg=default"
+        set -g pane-border-style            "fg=brightblack,bg=default"
+        set -g status-style                 "bg=default"
+        set -g window-status-current-format "#[fg=magenta]#W"
+        set -g window-status-format         "#[fg=gray]#W"
+
+        bind '%' split-window -c '#{pane_current_path}' -h
+        bind '"' split-window -c '#{pane_current_path}'
+        bind c   new-window   -c '#{pane_current_path}'
       '';
+
+      plugins = with pkgs.tmuxPlugins; [
+        better-mouse-mode
+        {
+          plugin = better-mouse-mode;
+          extraConfig = ''
+            set -g @scroll-without-changing-pane "on"
+            set -g @emulate-scroll-for-no-mouse-alternate-buffer "on"
+          '';
+        }
+        {
+          plugin = fzf-tmux-url;
+          extraConfig = ''
+            set -g @fzf-url-fzf-options '-w 50% -h 50% --prompt="   " --border-label=" Open URL " --no-preview'
+            set -g @fzf-url-history-limit 2000
+          '';
+        }
+      ];
     };
+
+    packages = with pkgs; [
+      gitmux
+    ];
   };
 }
+
