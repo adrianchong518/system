c0a9a5b4c38bd238cfe3ef86479375a72084db5d
Adrian Chong <adrianchong518@gmail.com>
2023-02-22 22:01:00 +0800

    feat(darwin): add playcover app

---
+++ modules/_darwin/desktop/apps/playcover.nix
@@ -0,0 +1,18 @@
+{ config, options, pkgs, lib, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.darwin.desktop.apps.playcover;
+in
+{
+  options.modules.darwin.desktop.apps.playcover = with types; {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    homebrew = {
+      taps = [ "PlayCover/playcover" ];
+      casks = [ "playcover-community" ];
+    };
+  };
+}
