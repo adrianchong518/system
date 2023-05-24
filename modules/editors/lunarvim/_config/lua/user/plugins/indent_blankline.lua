85796f6b13eb233f3927df30aca461be88cb4680
Adrian Chong <adrianchong518@gmail.com>
2023-05-24 22:27:25 +0800

    fix(lvim): make indent_blanklines not use treesitter

--- modules/editors/lunarvim/_config/lua/user/plugins/indent_blankline.lua
+++ modules/editors/lunarvim/_config/lua/user/plugins/indent_blankline.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.setup = function()
   lvim.builtin.indentlines.options.buftype_exclude = { "help", "terminal", "nofile" }
+  lvim.builtin.indentlines.options.use_treesitter = false
 end
 
 return M
