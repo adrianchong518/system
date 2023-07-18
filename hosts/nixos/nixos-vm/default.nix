ede81e407062c4d0d70d7522766dbf7a2fa45f10
Adrian Chong <adrianchong518@gmail.com>
2023-07-18 11:25:44 +0800

    feat(nixos): simple configuration

---
+++ hosts/nixos/nixos-vm/default.nix
@@ -0,0 +1,66 @@
+{ inputs, config, pkgs, lib, ... }:
+
+{
+  modules = {
+    desktop = {
+      enable = true;
+      browsers.firefox.enable = true;
+      term.alacritty.enable = true;
+      office.libreoffice.enable = true;
+
+      # communication = {
+      #   discord.enable = true;
+      #   zoom.enable = true;
+      # };
+
+      # apps = {
+      #   gimp.enable = true;
+      #   obsidian.enable = true;
+      # };
+    };
+
+    shell = {
+      fish.enable = true;
+      default = config.modules.shell.fish.package;
+
+      tmux.enable = true;
+
+      starship.enable = true;
+      utils.enable = true;
+
+      git = {
+        lazygit.enable = true;
+
+        signing = {
+          # enable = true;
+          key = "0CF62CE0";
+        };
+      };
+    };
+
+    editors = {
+      # vscode.enable = true;
+      lunarvim.enable = false;
+      neovim = {
+        enable = true;
+        config = "nvchad";
+      };
+      default = "nvim";
+    };
+
+    services = {
+      ssh = {
+        enable = true;
+        git = {
+          # enable = true;
+          # identityFile = "~/.ssh/id_ed25519_git";
+        };
+      };
+      gpg.enable = true;
+    };
+
+    dev = {
+      # platformio.enable = true;
+    };
+  };
+}
--- modules/_nixos/default.nix
+++ modules/_nixos/default.nix
@@ -11,5 +11,42 @@ with lib.my;
 
   system.stateVersion = "23.05";
 
-  user.extraGroups = [ "wheel" ];
+  user = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  time.timeZone = "Hongkong";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.xserver = {
+    # Enable the X11 windowing system.
+    enable = true;
+    desktopManager.xfce.enable = true;
+    displayManager.defaultSession = "xfce";
+
+    # Configure keymap in X11
+    layout = "us";
+    xkbVariant = "colemak";
+    xkbOptions = "caps:escape";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Gnome keyring
+  services.gnome.gnome-keyring.enable = true;
+
+  # vscode-server
+  services.vscode-server.enable = true;
 }
