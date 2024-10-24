2ebbd8b4acad9d9f7eda4cb1041c5dd0c69d534a
Adrian Chong <adrianchong518@gmail.com>
2024-10-24 17:01:01 +0800

    add: vesktop

--- modules/desktop/communication/discord.nix
+++ modules/desktop/communication/discord.nix
@@ -2,11 +2,14 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.communication.discord;
-in {
+let
+  inherit (pkgs.stdenvNoCC) isLinux;
+  cfg = config.modules.desktop.communication.discord;
+in
+{
   options.modules.desktop.communication.discord = with types; {
     enable = mkBoolOpt false;
   };
 
-  config = mkIf cfg.enable { packages = [ pkgs.discord ]; };
+  config = mkIf cfg.enable { packages = [ (if isLinux then pkgs.vesktop else pkgs.discord) ]; };
 }
