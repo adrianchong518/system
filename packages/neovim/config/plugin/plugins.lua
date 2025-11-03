accbfe62dbfce2afdfbc210c55dacd1d21293ffc
Adrian Chong <adrianchong518@gmail.com>
2025-11-03 17:44:32 +0100

    fix(neovim): neogit error and surround/leap keymap conflict

--- packages/neovim/config/plugin/plugins.lua
+++ packages/neovim/config/plugin/plugins.lua
@@ -1,7 +1,6 @@
 -- many plugins annoyingly require a call to a 'setup' function to be loaded,
 -- even with default configs
 
-require("nvim-surround").setup()
 require("which-key").setup()
 require("auto-hlsearch").setup()
 require("crates").setup()
---
+++ packages/neovim/config/plugin/surround.lua
@@ -0,0 +1,10 @@
+require("nvim-surround").setup {
+  keymaps = {
+    normal = "yz",
+    delete = "dz",
+    visual = "Z",
+    visual_line = "gZ",
+    change = "cz",
+    change_line = "cZ",
+  }
+}
