0799d6c79b893357387d54eca3ec081dc9913902
Adrian Chong <adrianchong518@gmail.com>
2026-02-06 19:31:05 +0800

    add(neovim): force format on save for typst files

--- packages/neovim/config/ftplugin/typst.lua
+++ packages/neovim/config/ftplugin/typst.lua
@@ -1,1 +1,2 @@
 require('user').set_indent_width(2)
+require('user').format_on_save(0, nil)
--- packages/neovim/config/lua/user/init.lua
+++ packages/neovim/config/lua/user/init.lua
@@ -17,4 +17,14 @@ function M.add_mini_clue(clue)
   table.insert(require('mini.clue').config.clues, clue)
 end
 
+function M.format_on_save(bufnr, client_id)
+  vim.api.nvim_create_autocmd('BufWritePre', {
+    group = vim.api.nvim_create_augroup('my.lsp', { clear = false, }),
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({ bufnr = bufnr, id = client_id, timeout_ms = 1000, })
+    end,
+  })
+end
+
 return M
