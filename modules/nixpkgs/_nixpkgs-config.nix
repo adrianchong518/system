62d67ad7eb98a92b4f7d92ec57952f48b123d6b9
Adrian Chong <adrianchong518@gmail.com>
2026-08-27 00:34:35 +0800

    fix(nix): remove insecure package

--- modules/nixpkgs/_nixpkgs-config.nix
+++ modules/nixpkgs/_nixpkgs-config.nix
@@ -4,7 +4,6 @@
   allowBroken = false;
   nvidia.acceptLicense = true;
   permittedInsecurePackages = [
-    "electron-39.8.10" # XXX: bitwarden desktop
     "electron-40.10.5" # XXX: winboat
   ];
 }
