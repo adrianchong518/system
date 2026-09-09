e903ce3698801c345e234e959fe84582ba690370
Adrian Chong <adrianchong518@gmail.com>
2026-09-09 22:05:02 +0800

    add(neovim): config vimtex quickfix list

--- packages/neovim/config/plugin/vimtex.lua
+++ packages/neovim/config/plugin/vimtex.lua
@@ -6,3 +6,9 @@ vim.g.vimtex_callback_progpath = vim.fn.exepath(vim.v.progname)
 vim.g.vimtex_compiler_latexmk = {
   ['out_dir'] = 'build',
 }
+
+vim.g.vimtex_quickfix_ignore_filters = {
+  'Token not allowed in a PDF string',
+}
+
+vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
